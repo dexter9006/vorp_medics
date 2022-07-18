cd3b88359e4827a12d60986f5e37c26e7b227e88
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-07-18 18:47:32 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,19 +2,16 @@ fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 game 'rdr3'
-version '1.0'
-author 'YourgenAP'
-
-client_scripts { 
-    'client/UiServices.lua',
-    'client/MedicineControllers.lua',
-    'client/MedicineHandlers.lua',
-    'client/MedicineMenu.lua'
+author 'VORP @YourgenAP'
+lua54 'yes'
+description 'Medical VORP script'
+client_scripts {
+    'client/*.lua',
 }
 
-server_scripts { 
-    'server/MedicineControllers.lua',
-    'server/MedicineHandlers.lua'
+server_scripts {
+    'server/*.lua',
+
 }
 
 shared_scripts {
@@ -32,3 +29,9 @@ dependencies {
     'vorp_crafting',
     'menuapi'
 }
+
+--dont touch
+--version '1.3'
+--vorp_checker 'yes'
+--vorp_name '^4Resource version Check^3'
+--vorp_github 'https://github.com/VORPCORE/vorp_medics'
